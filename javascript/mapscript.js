4beedae0b3ff6e9ab9f5a94efadb56096ee30f90
bodette-dev <54686614+bodette-dev@users.noreply.github.com>
2019-11-13 23:02:08 -0600

    Update mapscript.js

--- javascript/mapscript.js
+++ javascript/mapscript.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.742951, lng: -87.733638},
-    zoom: 8
+    zoom: 50
   });
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
