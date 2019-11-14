8b136baf9683e9f0d7f1192d94db18e69d6e3042
bodette-dev <54686614+bodette-dev@users.noreply.github.com>
2019-11-14 00:13:21 -0600

    Update mapscript.js

--- javascript/mapscript.js
+++ javascript/mapscript.js
@@ -6,7 +6,9 @@ function initMap() {
   });
   // The marker, positioned at Uluru
   var start = {lat: 41.742951, lng: -87.733638};
+  var workStart = {lat: 41.724962, lng: -87.741513};
   var marker = new google.maps.Marker({position: start, map: map});
+  var workMarker = new google.maps.Marker({position: start, map: map});
   
   var contentString = '<div id="content">'+
       '<div id="siteNotice">'+
@@ -17,12 +19,29 @@ function initMap() {
       '</div>'+
       '</div>';
   
+  var workString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">First Job</h1>'+
+      '<div id="bodyContent">'+
+      '<img class="mapPics" src ="media/work.png">'+
+      '</div>'+
+      '</div>';
+  
   var homeWindow = new google.maps.InfoWindow({
     content: contentString
+  });
+  
+  var workWindow = new google.maps.InfoWindow({
+    content: workString
   });
   
   marker.addListener('click', function() {
     map.setCenter(marker.getPosition());
     homeWindow.open(map, marker);
   });
+  
+  workMarker.addListener('click',function() {
+    map.setCenter(workMarker.getPosition());
+    worrkWindow.open(map, marker);
 }
