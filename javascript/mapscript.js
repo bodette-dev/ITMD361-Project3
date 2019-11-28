8fc08b5afa56823b699e81cfd8c7b0249f74ec4b
bodette-dev <54686614+bodette-dev@users.noreply.github.com>
2019-11-28 13:44:37 -0600

    Update mapscript.js

--- javascript/mapscript.js
+++ javascript/mapscript.js
@@ -75,28 +75,28 @@ function initMap() {
   });
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var index = 1;
+openSlides(index);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function nextSlides(n) {
+  openSlides(index += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  openSlides(index = n);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+function openSlides(n) {
+  var x;
+  var images = document.getElementsByClassName("slides");
 
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  if (n > images.length) {index = 1} 
+  if (n < 1) {index = images.length}
+  for (x = 0; x < slides.length; ++x) {
+      images[x].style.display = "none"; 
   }
 
-  slides[slideIndex-1].style.display = "block"; 
+  images[index-1].style.display = "block"; 
 }
